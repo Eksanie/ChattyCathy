4ea0b21e942653c8d3ce8b1e359f1dafd37b2b4f
Eksanie <mghattas3@gmail.com>
2020-01-29 10:15:48 -0800

    added takePic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, {useState,useEffect} from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 import {db, useDB} from './db'
 import NamePicker from './NamePicker'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Camera from 'react-snap-pic'
+import {FiCamera} from 'react-icons/fi'
+import * as firebase from "firebase/app"
+import "firebase/storage"
 
 function App(){
   useEffect(()=>{
@@ -14,48 +18,85 @@ function App(){
   </BrowserRouter>
 }
 
+
 function Room(props) {
   const {room} = props.match.params
   const [name, setName] = useState('')
+  const [showCamera, setShowCamera] = useState(false)
   const messages = useDB(room)
 
+  async function takePicture(img) {
+    setShowCamera(false)
+    const imgID = Math.random().toString(36).substring(7)
+    var storageRef = firebase.storage().ref()
+    var ref = storageRef.child(imgID + '.jpg')
+    await ref.putString(img, 'data_url')
+    db.send({ 
+      img: imgID, name, ts: new Date(), room 
+    })
+  }
+
   return <main>
+
+    {showCamera && <Camera takePicture={takePicture} />}
 
     <header>
       <div className="logo-wrap">
         <img className="logo"
           alt="logo"
           src="https://icon.community/static/iconsensus/img/logos/og-img_1.png"/>
+
         ChattyCathy
       </div>
       <NamePicker onSave={setName} />
     </header>
 
     <div className="messages">
-      {messages.map((m,i)=>{
-        return <div key={i} className="message-wrap"
-          from={m.name===name?'me':'you'}>
-          <div className="message">
-            <div className="msg-name">{m.name}</div>
-            <div className="msg-text">{m.text}</div>
-          </div>
-        </div>
-      })}
+      {messages.map((m,i)=> <Message key={i} 
+        m={m} name={name} 
+      />)}
     </div>
 
-    <TextInput onSend={(text)=> {
-      db.send({
-        text, name, ts: new Date(), room
-      })
-    }} />
+    <TextInput 
+      showCamera={()=>setShowCamera(true)}
+      onSend={(text)=> {
+        db.send({
+          text, name, ts: new Date(), room
+        })
+      }} 
+    />
     
   </main>
 }
 
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/chattycathy2025.appspot.com/o/'
+const suffix = '.jpg?alt=media'
+
+function Message({m, name}){
+  return <div className="message-wrap"
+    from={m.name===name?'me':'you'}
+    onClick={()=>console.log(m)}>
+    <div className="message">
+      <div className="msg-name">{m.name}</div>
+      <div className="msg-text">
+        {m.text}
+        {m.img && <img src={bucket + m.img + suffix} alt="pic" />}
+      </div>
+    </div>
+  </div>
+}
+
+
+
 function TextInput(props){
   var [text, setText] = useState('') 
+
   // normal js comment
   return <div className="text-input-wrap">
+    <button onClick={props.showCamera}
+      style={{position:'absolute', left:2, top:10}}>
+      <FiCamera style={{height:15, width:15}} />
+    </button>
     <input 
       value={text} 
       className="text-input"
