340fa71d008d263d57529a89a6e408e136ff47c9
Eksanie <mghattas3@gmail.com>
2020-01-29 08:45:08 -0800

    pushing again

--- src/App.js
+++ src/App.js
@@ -1,38 +1,81 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import {db, useDB} from './db'
+import NamePicker from './namePicker'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-function App() {
+function App(){
+  useEffect(()=>{
+    const {pathname} = window.location
+    if(pathname.length<2) window.location.pathname='home'
+  }, [])
+  return <BrowserRouter>
+    <Route path="/:room" component={Room} />
+  </BrowserRouter>
+}
+
+function Room(props) {
+  const {room} = props.match.params
+  const [name, setName] = useState('')
+  const messages = useDB(room)
+
   return <main>
 
-
-    <header>  
-
-    <img src= "http://cdn.bleacherreport.net/images_root/slides/photos/000/462/711/62wash_display_image.jpg?1287961269"/>
-    
-    Chatter 
+    <header>
+      <div className="logo-wrap">
+        <img className="logo"
+          alt="logo"
+          src="http://cdn.bleacherreport.net/images_root/slides/photos/000/462/711/62wash_display_image.jpg?1287961269"/>
+        Chatter
+      </div>
+      <NamePicker onSave={setName} />
     </header>
-  
-    <TextInput onSend={m=> console.log(m)} />
 
+    <div className="messages">
+      {messages.map((m,i)=>{
+        return <div key={i} className="message-wrap"
+          from={m.name===name?'me':'you'}>
+          <div className="message">
+            <div className="msg-name">{m.name}</div>
+            <div className="msg-text">{m.text}</div>
+          </div>
+        </div>
+      })}
+    </div>
+
+    <TextInput onSend={(text)=> {
+      db.send({
+        text, name, ts: new Date(), room
+      })
+    }} />
+    
   </main>
 }
 
 function TextInput(props){
-  var [text, setText] = useState('')
-  return <div className = "text-input">
-  <input value = {text} 
-    placeholder = "write your message"
-    onChange = {e => setText(e.target.value)}
-  
-  />
-  <button onClick={() => {
-  props.onSend(text)
-  setText('')
-  }}>
-  SEND
-  </button>
-
+  var [text, setText] = useState('') 
+  // normal js comment
+  return <div className="text-input-wrap">
+    <input 
+      value={text} 
+      className="text-input"
+      placeholder="write your message"
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={e=> {
+        if(e.key==='Enter') {
+          if(text) props.onSend(text)
+          setText('')
+        }
+      }}
+    />
+    <button onClick={()=> {
+      if(text) props.onSend(text)
+      setText('')
+    }} className="button"
+      disabled={!text}>
+      SEND
+    </button>
   </div>
 }
 
-export default App;
+export default App
--- src/db.js
+++ src/db.js
@@ -1,7 +1,56 @@
-{
-  /* Visit https://firebase.google.com/docs/database/security to learn more about security rules. */
-  "rules": {
-    ".read": false,
-    ".write": false
-  }
+import {useState, useEffect} from 'react'
+import * as firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/storage"
+
+let store
+const coll = 'messages'
+
+function useDB(room) {
+    const [messages, setMessages] = useState([])
+
+    function add(m) {
+        setMessages(current => {
+            const msgs = [m, ...current]
+            msgs.sort((a,b)=> b.ts.seconds - a.ts.seconds)
+            return msgs
+        })
+    }
+    function remove(id) {
+        setMessages(current=> current.filter(m=> m.id!==id))
+    }
+    
+    useEffect(() => {
+        store.collection(coll)
+        .where('room','==',room)
+        .onSnapshot(snap=> snap.docChanges().forEach(c=> {
+            const {doc, type} = c
+            if (type==='added') add({...doc.data(),id:doc.id})
+            if (type==='removed') remove(doc.id)
+        }))
+    }, [])
+    return messages
 }
+
+const db = {}
+db.send = function(msg) {
+    return store.collection(coll).add(msg)
+}
+db.delete = function(id) {
+    return store.collection(coll).doc(id).delete()
+}
+
+export { db, useDB }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5VQKLa_X2nuncYmgyrwwEDFyZjTLH_tQ",
+  authDomain: "chattycathy2025.firebaseapp.com",
+  databaseURL: "https://chattycathy2025.firebaseio.com",
+  projectId: "chattycathy2025",
+  storageBucket: "chattycathy2025.appspot.com",
+  messagingSenderId: "12664040371",
+  appId: "1:12664040371:web:1b92c6e2b9fa6ff2c8f9c6"
+};
+
+firebase.initializeApp(firebaseConfig)
+store = firebase.firestore()
